0304f4e6807c2b502d19c1057837c8952e00f07f
Winfry Nyarangi <w.nyarangi@webmasters.co.ke>
2025-09-04 14:54:57 +0300

    recommendation-service

--- app/api/deals.py
+++ app/api/deals.py
@@ -1,6 +1,6 @@
 # app/api/deals.py
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from typing import List
 
 from app.models.db_models import Deal
@@ -14,4 +14,4 @@ def get_all_deals(db: Session = Depends(get_db)):
     """
     Get a list of the first 10 deals.
     """
-    return db.query(Deal).limit(10).all()
+    return db.query(Deal).options(joinedload(Deal.created_by_user)).limit(10).all()
